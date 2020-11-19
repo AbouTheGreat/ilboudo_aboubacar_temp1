57f55075a63341fc4b6e614c2284ccc6d0949dcf
AbouTheGreat <72754592+AbouTheGreat@users.noreply.github.com>
2020-11-19 14:14:04 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-while True: 
-    print("Current Room Temperature: "+ input.temperature(TemperatureUnit.FAHRENHEIT)+"°F " +"- "+input.temperature(TemperatureUnit.CELSIUS)+"°C")
-    
-
+while True:
+    print("temperature: "+ input.temperature(TemperatureUnit.FAHRENHEIT))
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 70:  
+        light.set_all(light.rgb(255,0,0))
+    elif 40 < input.temperature(TemperatureUnit.FAHRENHEIT) < 70:
+        light.set_all(light.rgb(0, 255, 0))
+    else:
+        light.set_all(light.rgb(0, 0, 255)
+        )
     
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 while (true) {
-    console.log("Current Room Temperature: " + input.temperature(TemperatureUnit.Fahrenheit) + "°F " + "- " + input.temperature(TemperatureUnit.Celsius) + "°C")
+    console.log("temperature: " + input.temperature(TemperatureUnit.Fahrenheit))
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 70) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else if (40 < input.temperature(TemperatureUnit.Fahrenheit) && input.temperature(TemperatureUnit.Fahrenheit) < 70) {
+        light.setAll(light.rgb(0, 255, 0))
+    } else {
+        light.setAll(light.rgb(0, 0, 255))
+    }
+    
 }
